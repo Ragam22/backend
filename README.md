481c3cc5addce0174856eb1ffd50033040119ade
Cliford <clifordjo@gmail.com>
2021-04-22 23:36:13 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Tathva'20 Backend
+
+The backend for the Tathva'20 website made in Strapi.
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '14cb7d67-fee2-4f4b-9ac5-501f24bd2ea2'
+};
