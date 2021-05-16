d1205c95115e89b2c9add3dbb87f9df7ea7666df
Cliford <clifordjo@gmail.com>
2021-05-16 16:25:08 +0530

    Fixed bugs and added formatting

--- api/user-event-detail/controllers/user-event-detail.js
+++ api/user-event-detail/controllers/user-event-detail.js
@@ -1,178 +1,240 @@
-'use strict';
+"use strict";
 
-const { sanitizeEntity } = require('strapi-utils');
+const { sanitizeEntity } = require("strapi-utils");
 
 module.exports = {
-	async create(ctx) {
+  async create(ctx) {
+    const { user, event, refCode } = ctx.request.body;
 
-		const { user, event, refCode } = ctx.request.body;
+    if (user.id !== ctx.state.user.id)
+      return ctx.unauthorized("Unauthorized user access");
 
-		if (user.id !== ctx.state.user.id)
-			return ctx.unauthorized("Unauthorized user access");
+    const hasUserRegistered = ctx.state.user["registeredEvents"].find(
+      (o) => o.event === event.id
+    );
+    if (typeof hasUserRegistered !== "undefined")
+      return ctx.badRequest("Already registered for event");
 
-		const hasUserRegistered = ctx.state.user['registeredEvents'].find(o => o.event === event.id)
-		if (typeof hasUserRegistered !== 'undefined')
-			return ctx.badRequest("Already registered for event");
+    let eventObj = await strapi.services.event.findOne({ id: event.id });
 
-		let eventObj = await strapi.services.event.findOne({ id: event.id });
+    if (eventObj === null) return ctx.badRequest("Event does not exist");
 
-		if (eventObj === null)
-			return ctx.badRequest('Event does not exist');
+    if (eventObj.regPrice !== 0) return ctx.badRequest("This is a paid event");
 
-		if(eventObj.regPrice !== 0)
-			return ctx.badRequest('This is a paid event');
+    let currentDate = new Date();
+    if (
+      !(
+        new Date(eventObj.regStartDate) < currentDate &&
+        currentDate < new Date(eventObj.regEndDate)
+      )
+    )
+      return ctx.badRequest("Not in registration period");
 
-		let currentDate = new Date();
-		if (!(new Date(eventObj.regStartDate) < currentDate && currentDate < new Date(eventObj.regEndDate)))
-			return ctx.badRequest('Not in registration period');
+    const updateData = {
+      teamMembers: [{ id: user.id }],
+      event,
+      status: "participating",
+      eventRefCode: refCode,
+    };
 
-		const updateData = {
-			teamMembers: [{ id: user.id }],
-			event,
-			status: 'participating',
-			eventRefCode : refCode
-		};
+    await strapi
+      .query("event")
+      .model.query((qb) => {
+        qb.where("id", eventObj.id);
+        qb.increment("currentRegCount", 1);
+      })
+      .fetch();
 
-		await strapi.query("event").model.query((qb) => {
-            qb.where("id", eventObj.id);
-            qb.increment("currentRegCount", 1);
-		}).fetch();
-		 
-		let entity = await strapi.services['user-event-detail'].create(updateData);
-		return sanitizeEntity(entity, { model: strapi.models['user-event-detail'] });
-	},
+    let entity = await strapi.services["user-event-detail"].create(updateData);
+    return sanitizeEntity(entity, {
+      model: strapi.models["user-event-detail"],
+    });
+  },
 
-	async findOne(ctx) {
-		const paramId = Number.parseInt(ctx.params.id, 10);
+  async findOne(ctx) {
+    const paramId = Number.parseInt(ctx.params.id, 10);
 
-		if (typeof (ctx.state.user['registeredEvents'].find(o => o.id === paramId)) === 'undefined')
-			return ctx.unauthorized("Not a part of this team");
+    if (
+      typeof ctx.state.user["registeredEvents"].find(
+        (o) => o.id === paramId
+      ) === "undefined"
+    )
+      return ctx.unauthorized("Not a part of this team");
 
-		const detail = await strapi.services['user-event-detail'].findOne({ id: paramId });
-		const event = await strapi.services.event.findOne({ id: detail.event.id });
-        
-		if(Array.isArray(event.commonMetaValues))
-			detail.metaValues = event.commonMetaValues.concat((Array.isArray(detail.metaValues))?detail.metaValues:[]);
+    const detail = await strapi.services["user-event-detail"].findOne({
+      id: paramId,
+    });
+    const event = await strapi.services.event.findOne({ id: detail.event.id });
 
-		if(Array.isArray(detail.userResponses)){
-			for (let res of detail.userResponses){
-				if(typeof res.response === 'number')
-					res.response = detail.submissions.find(o => o.id === res.response);
-			}
-		}
-		
-		delete detail.submissions;
-		delete detail.created_by;
-		delete detail.updated_by;
-		delete detail.created_at;
-		delete detail.updated_at;
-		delete detail.published_at;
-		return detail;
-	},
+    if (Array.isArray(event.commonMetaValues))
+      detail.metaValues = event.commonMetaValues.concat(
+        Array.isArray(detail.metaValues) ? detail.metaValues : []
+      );
 
-	async update(ctx) {
-		let paramId = Number.parseInt(ctx.params.id, 10);
-		let eventDetail = ctx.state.user['registeredEvents'].find(o => o.id === paramId);
+    if (Array.isArray(detail.userResponses)) {
+      for (let res of detail.userResponses) {
+        if (typeof res.response === "number")
+          res.response = detail.submissions.find((o) => o.id === res.response);
+      }
+    }
 
-		if (typeof eventDetail === 'undefined')
-			return ctx.unauthorized("Not a part of this team");
+    delete detail.submissions;
+    delete detail.created_by;
+    delete detail.updated_by;
+    delete detail.created_at;
+    delete detail.updated_at;
+    delete detail.published_at;
+    return detail;
+  },
 
-		if (eventDetail.status !== 'participating')
-			return ctx.unauthorized('Cannot edit this field');
+  async update(ctx) {
+    let paramId = Number.parseInt(ctx.params.id, 10);
+    let eventDetail = ctx.state.user["registeredEvents"].find(
+      (o) => o.id === paramId
+    );
 
-		const { teamMembers, userResponses } = ctx.request.body;
+    if (typeof eventDetail === "undefined")
+      return ctx.unauthorized("Not a part of this team");
 
-		let eventObj = await strapi.services.event.findOne({ id: eventDetail.event.id });
-		let currentDate = new Date();	
-		let updateData = {}
+    if (eventDetail.status !== "participating")
+      return ctx.unauthorized("Cannot edit this field");
 
-		if (new Date(eventObj.regStartDate) < currentDate && currentDate < new Date(eventObj.regEndDate) && Array.isArray(teamMembers)){
+    const { teamMembers, userResponses } = ctx.request.body;
 
-			if (teamMembers.length > eventObj.maxTeamSize || teamMembers.length < 1)
-				return ctx.badRequest("Invalid team size");
+    let eventObj = await strapi.services.event.findOne({
+      id: eventDetail.event,
+    });
+    let currentDate = new Date();
+    let updateData = {};
 
-			for (const mem of teamMembers) {
-				//if already a part of team
-				if (typeof (eventDetail.teamMembers.find(o => o.id === mem.id)) !== 'undefined')
-					continue;
+    if (
+      new Date(eventObj.regStartDate) < currentDate &&
+      currentDate < new Date(eventObj.regEndDate) &&
+      Array.isArray(teamMembers)
+    ) {
+      if (teamMembers.length > eventObj.maxTeamSize || teamMembers.length < 1)
+        return ctx.badRequest("Invalid team size");
 
-				//new user
-				let found = await strapi.query('user', 'users-permissions').findOne({ id: mem.id });
+      for (const mem of teamMembers) {
+        //if already a part of team
+        if (
+          typeof eventDetail.teamMembers.find((o) => o.id === mem.id) !==
+          "undefined"
+        )
+          continue;
 
-				if (found === null)
-					return ctx.badRequest("Invalid team member id");
+        //new user
+        let found = await strapi
+          .query("user", "users-permissions")
+          .findOne({ id: mem.id });
 
-				if (typeof (found['registeredEvents'].find(o => o.event === eventDetail.event.id)) !== 'undefined')
-					return ctx.badRequest("TathvaID " + found.tathvaId + " has already registered for this event.");
-			}
+        if (found === null) return ctx.badRequest("Invalid team member id");
 
-			updateData.teamMembers = teamMembers;
-		}
+        if (
+          typeof found["registeredEvents"].find(
+            (o) => o.event === eventDetail.event.id
+          ) !== "undefined"
+        )
+          return ctx.badRequest(
+            "TathvaID " +
+              found.tathvaId +
+              " has already registered for this event."
+          );
+      }
 
-		eventDetail = await strapi.services['user-event-detail'].findOne({ id: eventDetail.id });		//get full detail
+      updateData.teamMembers = teamMembers;
+    }
 
-		if(Array.isArray(userResponses) && new Date(eventObj.submissionStartDate) < currentDate && currentDate < new Date(eventObj.submissionEndDate)){
-			updateData.userResponses = eventDetail.userResponses;
-			if(updateData.userResponses === null)
-				updateData.userResponses = []
+    eventDetail = await strapi.services["user-event-detail"].findOne({
+      id: eventDetail.id,
+    }); //get full detail
 
-			for(const res of userResponses){
-				const {resId, response} = res;
+    if (
+      Array.isArray(userResponses) &&
+      new Date(eventObj.submissionStartDate) < currentDate &&
+      currentDate < new Date(eventObj.submissionEndDate)
+    ) {
+      updateData.userResponses = eventDetail.userResponses;
+      if (updateData.userResponses === null) updateData.userResponses = [];
 
-				let newResponse =  updateData.userResponses.find(o => o.resId === resId);
-				if(typeof newResponse === 'undefined'){
-					newResponse = {
-						resId: resId,
-						response: response
-					};
-					updateData.userResponses.push(newResponse);
-				}
+      for (const res of userResponses) {
+        const { resId, response } = res;
 
-				const subInfo = eventObj.submissionInfo.find(o => o.id === resId);
-				 
-				if(subInfo.submissionType === 'textInput' && typeof response !== 'string'){
-					return ctx.badRequest("Invalid response type");
+        let newResponse = updateData.userResponses.find(
+          (o) => o.resId === resId
+        );
+        if (typeof newResponse === "undefined") {
+          newResponse = {
+            resId: resId,
+            response: response,
+          };
+          updateData.userResponses.push(newResponse);
+        }
 
-				} else if (subInfo.submissionType == 'fileUpload') {
-					if(typeof eventDetail.submissions.find(o => o.id === response) === 'undefined')
-						return ctx.badRequest("Invalid submission id");                         
+        const subInfo = eventObj.submissionInfo.find((o) => o.id === resId);
 
-					newResponse.response = response;		//only edit responses for submissions
-				}
-			}
-		}
+        if (
+          subInfo.submissionType === "textInput" &&
+          typeof response !== "string"
+        ) {
+          return ctx.badRequest("Invalid response type");
+        } else if (subInfo.submissionType == "fileUpload") {
+          if (
+            typeof eventDetail.submissions.find((o) => o.id === response) ===
+            "undefined"
+          )
+            return ctx.badRequest("Invalid submission id");
 
-		await strapi.services['user-event-detail'].update({ id: eventDetail.id }, updateData);
-		return updateData;
-	},
+          newResponse.response = response; //only edit responses for submissions
+        }
+      }
+    }
 
-	async delete(ctx) {
-		let paramId = Number.parseInt(ctx.params.id, 10);
-		const eventDetail = ctx.state.user['registeredEvents'].find(o => o.id === paramId);
+    await strapi.services["user-event-detail"].update(
+      { id: eventDetail.id },
+      updateData
+    );
+    return updateData;
+  },
 
-		if (typeof eventDetail === 'undefined')
-			return ctx.unauthorized("Not a part of this team");
+  async delete(ctx) {
+    let paramId = Number.parseInt(ctx.params.id, 10);
+    const eventDetail = ctx.state.user["registeredEvents"].find(
+      (o) => o.id === paramId
+    );
 
-		const savedSubmissions = (await strapi.services['user-event-detail'].findOne({ id: eventDetail.id })).submissions;
-		for (const sub of savedSubmissions)
-			await strapi.plugins['upload'].services.upload.remove(sub);
+    if (typeof eventDetail === "undefined")
+      return ctx.unauthorized("Not a part of this team");
 
-		const detail = await strapi.services['user-event-detail'].delete({ id: eventDetail.id });
-		detail.metaValues = null;
-		return sanitizeEntity(detail, { model: strapi.models['user-event-detail'] });
-	},
+    const savedSubmissions = (
+      await strapi.services["user-event-detail"].findOne({ id: eventDetail.id })
+    ).submissions;
+    for (const sub of savedSubmissions)
+      await strapi.plugins["upload"].services.upload.remove(sub);
 
-	async deleteSubmission(ctx) {
-		const detailId = Number.parseInt(ctx.params.detailId);
-		const subId = Number.parseInt(ctx.params.submissionId);
+    const detail = await strapi.services["user-event-detail"].delete({
+      id: eventDetail.id,
+    });
+    detail.metaValues = null;
+    return sanitizeEntity(detail, {
+      model: strapi.models["user-event-detail"],
+    });
+  },
 
-		const eventDetail = await strapi.services['user-event-detail'].findOne({ id: detailId });
+  async deleteSubmission(ctx) {
+    const detailId = Number.parseInt(ctx.params.detailId);
+    const subId = Number.parseInt(ctx.params.submissionId);
 
-		if(typeof eventDetail.submissions.find(o => o.id === subId) === 'undefined')
-			return ctx.badRequest("Invalid submission id");                         
+    const eventDetail = await strapi.services["user-event-detail"].findOne({
+      id: detailId,
+    });
 
-		await strapi.plugins['upload'].services.upload.remove({id: subId});
-		return "Removed";
-	}
+    if (
+      typeof eventDetail.submissions.find((o) => o.id === subId) === "undefined"
+    )
+      return ctx.badRequest("Invalid submission id");
 
+    await strapi.plugins["upload"].services.upload.remove({ id: subId });
+    return "Removed";
+  },
 };
--- extensions/users-permissions/controllers/User.js
+++ extensions/users-permissions/controllers/User.js
@@ -1,106 +1,136 @@
-'use strict';
+"use strict";
 
-const { sanitizeEntity } = require('strapi-utils');
+const { sanitizeEntity } = require("strapi-utils");
 
 module.exports = {
-	async findOne(ctx) {
-		const tathvaId = ctx.params.id;
+  async findOne(ctx) {
+    const tathvaId = ctx.params.id;
 
-		const entity = await strapi.query('user', 'users-permissions').findOne({ tathvaId });
+    const entity = await strapi
+      .query("user", "users-permissions")
+      .findOne({ tathvaId });
 
-		if (entity === null)
-			return ctx.badRequest("Invalid ID");
-		const entity2 = { id: entity.id, name: entity.name, collegeName: entity.collegeName };
+    if (entity === null) return ctx.badRequest("Invalid ID");
+    const entity2 = {
+      id: entity.id,
+      name: entity.name,
+      collegeName: entity.collegeName,
+    };
 
-		return sanitizeEntity(entity2, { model: strapi.plugins['users-permissions'].models.user });
-	},
+    return sanitizeEntity(entity2, {
+      model: strapi.plugins["users-permissions"].models.user,
+    });
+  },
 
-	async update(ctx){
-		
-		const currentUserId = ctx.state.user.id;	
-	  
-		if (currentUserId !== Number.parseInt(ctx.params.id, 10)) {
-		  return ctx.unauthorized("Unable to edit this user");
-		}
-	  
-		// Extract the fields regular users should be able to edit
-		const { name, phoneNumber, collegeName, yearOfStudy, referralCode, state, district } = ctx.request.body;
+  async update(ctx) {
+    const currentUserId = ctx.state.user.id;
 
-		const updateData = {
-		  name,
-		  phoneNumber,
-		  collegeName,
-		  yearOfStudy,
-		  referralCode,
-		  state,
-		  district
-		};
-	  
-		Object.keys(updateData).forEach(key => updateData[key] === undefined && delete updateData[key]);
-		let entity = await strapi.query('user', 'users-permissions').update({ id: currentUserId }, updateData);
-		
-		return sanitizeEntity(entity, { model: strapi.plugins['users-permissions'].models.user });
-	},
+    if (currentUserId !== Number.parseInt(ctx.params.id, 10)) {
+      return ctx.unauthorized("Unable to edit this user");
+    }
 
+    // Extract the fields regular users should be able to edit
+    const {
+      name,
+      phoneNumber,
+      collegeName,
+      yearOfStudy,
+      referralCode,
+      state,
+      district,
+    } = ctx.request.body;
 
-	async me(ctx){
-		const user = ctx.state.user;
+    const updateData = {
+      name,
+      phoneNumber,
+      collegeName,
+      yearOfStudy,
+      referralCode,
+      state,
+      district,
+    };
 
-		if (!user) {
-			return ctx.badRequest(null, [{ messages: [{ id: 'No authorization header was found' }] }]);
-		}
+    Object.keys(updateData).forEach(
+      (key) => updateData[key] === undefined && delete updateData[key]
+    );
+    let entity = await strapi
+      .query("user", "users-permissions")
+      .update({ id: currentUserId }, updateData);
 
-		const userObj = await strapi.query('user', 'users-permissions').findOne({ id: ctx.state.user.id });
+    return sanitizeEntity(entity, {
+      model: strapi.plugins["users-permissions"].models.user,
+    });
+  },
 
-		//required data
-		const filtered = {
-			id: 					userObj.id,
-			email: 					userObj.email,
-			name: 					userObj.name,
-			phoneNumber: 			userObj.phoneNumber,
-			tathvaId: 				userObj.tathvaId,
-			collegeName: 			userObj.collegeName,
-			yearOfStudy: 			userObj.yearOfStudy,
-			state: 					userObj.state,
-			district: 				userObj.district,
-			referralCode: 			userObj.referralCode,
-			registeredEvents: 		userObj.registeredEvents,
-			registeredWorkshops: 	userObj.registeredWorkshops,
-			registeredLectures: 	userObj.registeredLectures
-		}
+  async me(ctx) {
+    const user = ctx.state.user;
 
+    if (!user) {
+      return ctx.badRequest(null, [
+        { messages: [{ id: "No authorization header was found" }] },
+      ]);
+    }
 
-		for(let detail of filtered.registeredEvents){
-			let eventObj = await strapi.services['event'].findOne({ id: detail.event });
-			detail.event = {
-				id: eventObj.id,
-				name:eventObj.name,
-				description: eventObj.description,
-				submissionStartDate: eventObj.submissionStartDate,
-				submissionEndDate: eventObj.submissionEndDate,
-				coverImage: eventObj.coverImage
-			}
-			delete detail.submissions;
-		}
-		for(const detail of filtered.registeredWorkshops){
-			let workshopObj = await strapi.services['workshop'].findOne({ id: detail.workshop });
-			detail.workshop = {
-				id: workshopObj.id,
-				name:workshopObj.name,
-				description: workshopObj.description,
-				coverImage: workshopObj.coverImage
-			}
-		}
-		for(const detail of filtered.registeredLectures){
-			let lectureObj = await strapi.services['lecture'].findOne({ id: detail.lecture });
-			detail.lecture = {
-				id: lectureObj.id,
-				name:lectureObj.name,
-				description: lectureObj.description,
-				coverImage: lectureObj.coverImage
-			}
-		} 
+    const userObj = await strapi
+      .query("user", "users-permissions")
+      .findOne({ id: ctx.state.user.id });
 
-		return filtered;
-	}
+    //required data
+    const filtered = {
+      id: userObj.id,
+      email: userObj.email,
+      name: userObj.name,
+      phoneNumber: userObj.phoneNumber,
+      tathvaId: userObj.tathvaId,
+      collegeName: userObj.collegeName,
+      yearOfStudy: userObj.yearOfStudy,
+      state: userObj.state,
+      district: userObj.district,
+      referralCode: userObj.referralCode,
+      registeredEvents: userObj.registeredEvents,
+      registeredWorkshops: userObj.registeredWorkshops,
+      registeredLectures: userObj.registeredLectures,
+    };
+
+    for (let detail of filtered.registeredEvents) {
+      strapi.log.debug(JSON.stringify(detail));
+      let eventObj = await strapi.services["event"].findOne({
+        id: detail.event,
+      });
+      strapi.log.debug(JSON.stringify(eventObj));
+      detail.event = {
+        id: eventObj.id,
+        name: eventObj.name,
+        description: eventObj.description,
+        submissionStartDate: eventObj.submissionStartDate,
+        submissionEndDate: eventObj.submissionEndDate,
+        coverImage: eventObj.coverImage,
+      };
+      delete detail.submissions;
+    }
+    for (const detail of filtered.registeredWorkshops) {
+      let workshopObj = await strapi.services["workshop"].findOne({
+        id: detail.workshop,
+      });
+      detail.workshop = {
+        id: workshopObj.id,
+        name: workshopObj.name,
+        description: workshopObj.description,
+        coverImage: workshopObj.coverImage,
+      };
+    }
+    for (const detail of filtered.registeredLectures) {
+      let lectureObj = await strapi.services["lecture"].findOne({
+        id: detail.lecture,
+      });
+      detail.lecture = {
+        id: lectureObj.id,
+        name: lectureObj.name,
+        description: lectureObj.description,
+        coverImage: lectureObj.coverImage,
+      };
+    }
+
+    return filtered;
+  },
 };
