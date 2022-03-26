5d2eac50a27956bb86cad775508e8671cd382451
Amal P Mathews <amalpmathews2003@gmail.com>
2022-03-26 10:07:37 +0530

    database to starapi

--- config/database.js
+++ config/database.js
@@ -3,25 +3,25 @@ module.exports = ({ env }) => ({
   connections: {
     default: {
       connector: 'bookshelf',
-      // settings: {
-      //   client: 'postgres',
-      //   host: env('DATABASE_HOST', '127.0.0.1'),
-      //   port: env.int('DATABASE_PORT', 5432),
-      //   database: env('DATABASE_NAME', 'strapi'),
-      //   username: env('DATABASE_USERNAME', ''),
-      //   password: env('DATABASE_PASSWORD', ''),
-      // },
-      // options: {
-      //   ssl: false,
-      // },
-
       settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        client: 'postgres',
+        host: env('DATABASE_HOST', '127.0.0.1'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'ragamdb'),
+        username: env('DATABASE_USERNAME', ''),
+        password: env('DATABASE_PASSWORD', ''),
       },
       options: {
-        useNullAsDefault: true,
+        ssl: false,   
       },
+
+      // settings: {
+      //   client: 'sqlite',
+      //   filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+      // },
+      // options: {
+      //   useNullAsDefault: true,
+      // },
     },
   },
 });
