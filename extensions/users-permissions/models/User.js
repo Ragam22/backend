4af4e2fb1ee36b97a697dbcc37c771a445ae20bb
Cliford <clifordjo@gmail.com>
2022-04-24 13:40:07 +0530

    fixed amount paid not being set

--- extensions/users-permissions/models/User.js
+++ extensions/users-permissions/models/User.js
@@ -1,18 +1,18 @@
-'use strict';
+"use strict";
 
-const { customAlphabet } = require('nanoid');
-const nanoid = customAlphabet('ABCDEFGHIJKLMNOPQRSTUVWXYZ', 6);
+const { customAlphabet } = require("nanoid");
+const nanoid = customAlphabet("ABCDEFGHIJKLMNOPQRSTUVWXYZ", 6);
 
 module.exports = {
-
   lifecycles: {
     async afterCreate(result) {
-      while(true){
-        try{
-          await strapi.query('user', 'users-permissions').update({id:result.id}, {ragamId: "R22-"+nanoid()});
+      while (true) {
+        try {
+          await strapi
+            .query("user", "users-permissions")
+            .update({ id: result.id }, { ragamId: "R22-" + nanoid(), amountPaid: 0 });
           break;
-        } catch(err){
-        }
+        } catch (err) {}
       }
     },
   },
